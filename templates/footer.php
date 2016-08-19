ef8c473ac356235a615fb13b77d4e9f819232f9a
Hurricat <agilson42@gmail.com>
2016-08-19 11:20:14 -0400

    analytics

--- templates/footer.php
+++ templates/footer.php
@@ -1,25 +1,34 @@
-				</div>
-				<div class="col-sm-3 col-sm-offset-1">
-					<article>
-						<h3>About</h3>
-						<p>ChuChu is a Discord Bot written in Python 3.5 using the <a href="https://github.com/Rapptz/discord.py">discord.py</a> library.</p>
-						<p>For more information, see the <a href="/about">About Page</a>.</p>
-					</article>
-					<article>
-						<h3>Tags</h3>
-						<ul class="list-group">
-							<li class="list-group-item"><a href="/tagged/chuchu-bot">ChuChu Bot</a></li>
-							<li class="list-group-item"><a href="/tagged/site">Site</a></li>
-						</ul>
-					</article>
-				</div>
-			</div>
+	    </div>
+		<div class="col-sm-3 col-sm-offset-1">
+		    <article>
+			<h3>About</h3>
+			<p>ChuChu is a Discord Bot written in Python 3.5 using the <a href="https://github.com/Rapptz/discord.py">discord.py</a> library.</p>
+			<p>For more information, see the <a href="/about">About Page</a>.</p>
+		    </article>
+		    <article>
+			<h3>Tags</h3>
+			<ul class="list-group">
+			    <li class="list-group-item"><a href="/tagged/chuchu-bot">ChuChu Bot</a></li>
+			    <li class="list-group-item"><a href="/tagged/site">Site</a></li>
+			</ul>
+		    </article>
 		</div>
-		<footer class="footer">
+	    </div>
+	</div>
+	<footer class="footer">
             <div class="container">
                 <p class="text-muted">&copy; 2016 Anthony Gilson</p>
             </div>
         </footer>
+        <script>
+            (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+            })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+            ga('create', 'UA-82813029-1', 'auto');
+            ga('send', 'pageview');
+        </script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
         <script src="/js/bootstrap.min.js"></script>
     </body>
--- templates/header.php
+++ templates/header.php
@@ -1,14 +1,14 @@
 <!DOCTYPE html>
 <html>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<meta name="description" content="ChuChu Bot Reference">
-	<meta property="og:title" content="Ailurophiliac">
-	<meta property="og:description" content="ChuChu Bot Reference">
-	<meta property="og:url" content="http://ailurophiliac.com">
-	<meta property="og:image" content="http://ailurophiliac.com/images/richpreview.png">
-	<link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
-	<link href="/css/bootstrap.min.css" rel="stylesheet">
-    <link href="/css/custom.css" rel="stylesheet">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="ChuChu Bot Reference">
+        <meta property="og:title" content="Ailurophiliac">
+        <meta property="og:description" content="ChuChu Bot Reference">
+        <meta property="og:url" content="http://ailurophiliac.com">
+        <meta property="og:image" content="http://ailurophiliac.com/images/richpreview.png">
+        <link rel="shortcut icon" href="/images/favicon.ico" type="image/x-icon">
+        <link href="/css/bootstrap.min.css" rel="stylesheet">
+        <link href="/css/custom.css" rel="stylesheet">
 	<?php if (isset($title)): ?>
 		<title>Ailurophiliac - <?= htmlspecialchars($title) ?></title>
 	<?php else: ?>
@@ -34,21 +34,21 @@
                         <li><a href="/projects">Projects</a></li>
                         <li><a href="/contact">Contact</a></li>
                         <li><a href="/links">Links</a></li>
-						<?php
-							if ($_SESSION['id']) {
-								echo '<li><a href="/admin">Admin Home</a></li>';
-		                        echo '<li><a href="/admin/tags">Tags</a></li>';
-		                        echo '<li><a href="/admin/newpost">New Post</a></li>';
-		                        echo '<li><a href="/admin/logout">Log Out</a></li>';
-							}
-						?>
+			<?php
+			    if ($_SESSION['id']) {
+			        echo '<li><a href="/admin">Admin Home</a></li>';
+		                echo '<li><a href="/admin/tags">Tags</a></li>';
+		                echo '<li><a href="/admin/newpost">New Post</a></li>';
+		                echo '<li><a href="/admin/logout">Log Out</a></li>';
+			    }
+			?>
                     </ul>
                 </div>
             </div>
         </nav>
-		<div class="container">
-			<div class="row">
-				<div class="col-sm-8">
-					<?php if (isset($title)): ?>
-						<h1><?= htmlspecialchars($title) ?></h1>
-					<?php endif ?>
+	<div class="container">
+	    <div class="row">
+	        <div class="col-sm-8">
+	            <?php if (isset($title)): ?>
+	                <h1><?= htmlspecialchars($title) ?></h1>
+	            <?php endif ?>
